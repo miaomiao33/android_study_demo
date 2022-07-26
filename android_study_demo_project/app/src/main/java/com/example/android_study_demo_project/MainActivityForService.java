deca8a23d41b6198187cfcf2bc9d0425dcc72e4a
风风 <2187647288@qq.com>
2022-07-27 01:02:58 +0800

    动态注册

--- android_study_demo_project/app/src/main/java/com/example/android_study_demo_project/MainActivityForService.java
+++ android_study_demo_project/app/src/main/java/com/example/android_study_demo_project/MainActivityForService.java
@@ -2,6 +2,7 @@ package com.example.android_study_demo_project;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,6 +14,12 @@ public class MainActivityForService extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_for_servicer);
+
+        //Use java to register receiver
+        UpdateIpSelectCity updateIpSelectCity = new UpdateIpSelectCity();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ActionUtils.ACTIONS_EQUES_UPDATE_IP);
+        registerReceiver(updateIpSelectCity,filter);
     }
 
     //send service by static method to receiver
@@ -23,9 +30,17 @@ public class MainActivityForService extends AppCompatActivity {
                 new ComponentName("com.example.android_study_demo_project",
                         "com.example.android_study_demo_project.CustomReceiver"));
 
-        // 上面这一行在Android 7.0及以下版本不是必须的，但是Android 8.0或者更高版本，发送广播的条件更加严苛，必须添加这一行内容。
- // 创建的ComponentName实例化对象有两个参数，第1个参数是指接收广播类的包名，第2个参数是指接收广播类的完整类名。
+        // 上面这一行在Android 7.0及以下版本不是必须的，但是Android 8.0或者更高版本，
+        // 发送广播的条件更加严苛，必须添加这一行内容。
+        // 创建的ComponentName实例化对象有两个参数，第1个参数是指接收广播类的包名，第2个参数是指接收广播类的完整类名。
 
+        sendBroadcast(intent);
+    }
+
+    //send activity receiver
+    public void sendAction1(View view) {
+        Intent intent = new Intent();
+        intent.setAction(ActionUtils.ACTIONS_EQUES_UPDATE_IP);
         sendBroadcast(intent);
     }
 }
