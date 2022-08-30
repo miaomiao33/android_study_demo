d6b8b09400d47dcd513aad062ad1abfbe64498c0
风风 <2187647288@qq.com>
2022-08-31 02:14:00 +0800

    对接高德地图小蓝点，添加定位权限的动态申请判断，

--- android_study_demo_project/app/src/main/java/com/example/android_study_demo_project/MainActivity.java
+++ android_study_demo_project/app/src/main/java/com/example/android_study_demo_project/MainActivity.java
@@ -1,21 +1,78 @@
 package com.example.android_study_demo_project;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 //default activity
 public class MainActivity extends AppCompatActivity {
 
     private  final String TAG = MainActivity.class.getSimpleName();
+
+    //权限数组（申请定位）
+    private String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS};
+
+    //返回code
+    private static final int OPEN_SET_REQUEST_CODE = 100;
+    //调用此方法判断是否拥有权限
+    private void initPermissions() {
+        if (lacksPermission(permissions)) {//判断是否拥有权限
+            //请求权限，第二参数权限String数据，第三个参数是请求码便于在onRequestPermissionsResult 方法中根据code进行判断
+            ActivityCompat.requestPermissions(this, permissions, OPEN_SET_REQUEST_CODE);
+
+        } else {
+            //拥有权限执行操作
+        }
+    }
+
+    //如果返回true表示缺少权限
+        public boolean lacksPermission(String[] permissions) {
+        for (String permission : permissions) {
+            //判断是否缺少权限，true=缺少权限
+            if(ContextCompat.checkSelfPermission(getApplicationContext(), permission) != PackageManager.PERMISSION_GRANTED){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){//响应Code
+            case OPEN_SET_REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    for(int i = 0; i < grantResults.length; i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(getApplicationContext(),"未拥有相应权限"+i+"___"+grantResults[i],Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                    }
+                    //拥有权限执行操作
+                } else {
+                    Toast.makeText(getApplicationContext(),"未拥有相应权限3",Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,TAG+"--->onCreate");
+        initPermissions();
     }
 
     @Override
--- android_study_demo_project/app/src/main/java/com/example/android_study_demo_project/MainActivity_map.java
+++ android_study_demo_project/app/src/main/java/com/example/android_study_demo_project/MainActivity_map.java
@@ -2,27 +2,33 @@ package com.example.android_study_demo_project;
 //高德地图的显示activity
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.MapsInitializer;
 import com.amap.api.maps.model.MyLocationStyle;
 
-public class MainActivity_map extends AppCompatActivity {
+public class MainActivity_map extends AppCompatActivity implements AMapLocationListener, LocationSource {
 
     private MapView mMapView = null;
     private AMap aMap = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_map);
-        //获取地图控件引用
-        mMapView = (MapView) findViewById(R.id.map);
         //在构造MapView之前必须进行合规检查（OfflineMapManager，LBSTraceClient等也是一样的操作），设置接口之前保证隐私政策合规，
         MapsInitializer.updatePrivacyShow(getApplicationContext(),true,true);
         MapsInitializer.updatePrivacyAgree(getApplicationContext(),true);
+        setContentView(R.layout.activity_main_map);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.map);
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mMapView.onCreate(savedInstanceState);
         //初始化地图控制器对象
@@ -31,17 +37,26 @@ public class MainActivity_map extends AppCompatActivity {
         }
         MyLocationStyle myLocationStyle;
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位且将视角移动到地图
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        // 设置定位监听
+        aMap.setLocationSource(this);
+        // 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        aMap.setMyLocationEnabled(true);
+        // 设置定位的类型为定位模式，有定位、跟随或地图根据面向方向旋转几种
+        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
         mMapView.onDestroy();
+        if(null != mlocationClient){
+            mlocationClient.onDestroy();
+        }
     }
 
     @Override
@@ -63,4 +78,63 @@ public class MainActivity_map extends AppCompatActivity {
         mMapView.onSaveInstanceState(outState);
     }
 
+    OnLocationChangedListener mListener;
+    AMapLocationClient mlocationClient;
+    AMapLocationClientOption mLocationOption;
+/**
+ * 激活定位
+ */
+@Override
+public void activate(OnLocationChangedListener listener) {
+    mListener = listener;
+    if (mlocationClient == null) {
+        //初始化定位
+        try {
+            mlocationClient = new AMapLocationClient(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位回调监听
+        mlocationClient.setLocationListener(this);
+        //设置为高精度定位模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置定位参数
+        mlocationClient.setLocationOption(mLocationOption);
+        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+        // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+        mlocationClient.startLocation();//启动定位
+    }
+}
+/**
+ * 停止定位
+ */
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
+
+    /**
+     * 定位成功后回调函数
+     */
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (mListener != null&&amapLocation != null) {
+            if (amapLocation != null
+                    &&amapLocation.getErrorCode() == 0) {
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+            } else {
+                String errText = "定位失败," + amapLocation.getErrorCode()+ ": " + amapLocation.getErrorInfo();
+                Log.e("AmapErr",errText);
+            }
+        }
+    }
 }
